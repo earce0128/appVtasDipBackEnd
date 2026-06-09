aa82439606514f3cc8343b4dfd66c8763d39c56c
earce0128 <edgararce@msn.com>
2026-06-09 12:54:57 -0600

    Primer Commit - Analisis Basico de Spring Boot, Dependencias iniciales y uso de application.properties

---
+++ src/main/java/mx/com/mxds/appvtas/AppVentasDipBkSpringApplication.java
@@ -0,0 +1,13 @@
+package mx.com.mxds.appvtas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppVentasDipBkSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppVentasDipBkSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/mxds/appvtas/AppVentasDipBkSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.mxds.appvtas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppVentasDipBkSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
